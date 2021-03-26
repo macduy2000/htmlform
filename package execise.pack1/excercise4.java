d3639760a7e0a29c52d2cd37618b1b8b8440360f
Duy Mac <duymac123456@gmail.com>
2021-03-26 20:43:04 +0700

    init

---
+++ package execise.pack1/excercise4.java
@@ -0,0 +1,18 @@
+// package com.excercise4.javaapplication;
+class Subject {
+    public int diemmon;
+    public int sotinchi;
+
+    // ktao ko tham so
+    public Subject() {
+
+    };
+
+    // ktao co tham so
+    public Subject(int diemmon, int sotinchi) {
+        this.diemmon = diemmon;
+        this.sotinchi = sotinchi;
+
+    }
+
+}
